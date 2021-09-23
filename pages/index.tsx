8060647983678d912654a271e8ba90673b88dadc
Tyler Sullberg <tysullberg@gmail.com>
2021-09-22 18:25:49 -0700

    Create shell of Contributions modal.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 import * as React from 'react';
+import ContributionModal from '../components/Contributions/ContributionsModal';
 
 import { useAccount, useContract } from '../shared/contexts';
 
@@ -7,14 +8,46 @@ const Title = styled.h1`
   color: purple;
 `;
 
+const ContributionModalButton = styled.button`
+  background-color: #85d59a;
+`;
+
 export const Home = (): JSX.Element => {
   const contract = useContract();
   const account = useAccount();
 
+  const [isOpen, setIsOpen] = React.useState(true);
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
   console.log('contract is ', contract);
   console.log('account is ', account);
 
-  return <Title>Station terminal</Title>;
+  return (
+    <>
+      <Title>Station terminal</Title>
+      <ContributionModalButton
+        onClick={() => {
+          setIsOpen(true);
+        }}
+      >
+        Contributions
+      </ContributionModalButton>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+      <ContributionModal isOpen={isOpen} onRequestClose={handleClose}>
+        <p>This is the contribution modal</p>
+      </ContributionModal>
+    </>
+  );
 };
 
 export default Home;
