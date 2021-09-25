37a7c9ca0fe045f7bbedbd46ab1b87736791059a
Tyler Sullberg <tysullberg@gmail.com>
2021-09-24 21:38:51 -0700

    Create contributor directory component.

--- pages/index.tsx
+++ pages/index.tsx
@@ -135,12 +135,63 @@ const ContributorsListWrapper = styled.div`
   height: 461px;
   width: 408px;
   margin-left: 10px;
+  overflow: scroll;
+`;
+
+const ContributorsListTitle = styled.h2`
+  margin: 20px;
+  font-family: Terminal;
+  font-size: 30px;
+`;
+
+const AliasesWrapper = styled.div`
+  column-count: 2;
+  margin: 0px 20px;
+`;
+
+const Alias = styled.div`
+  margin-bottom: 10px;
+  font-size: 14px;
+  font-family: Favorit Pro;
 `;
 
 const ContributorsList = (): JSX.Element => {
+  const aliases = [
+    '@mindapi',
+    '@fakepixels',
+    '@zaz',
+    '@scottsgc',
+    '@fullyallocated',
+    '@tsully',
+    '@mindapi',
+    '@fakepixels',
+    '@zaz',
+    '@scottsgc',
+    '@fullyallocated',
+    '@tsully',
+    '@mindapi',
+    '@fakepixels',
+    '@zaz',
+    '@scottsgc',
+    '@fullyallocated',
+    '@tsully',
+    '@mindapi',
+    '@fakepixels',
+    '@zaz',
+    '@scottsgc',
+    '@fullyallocated',
+    '@tsully',
+  ];
+
   return (
     <ContributorsListWrapper>
       <TopBar mandatory={false} />
+      <ContributorsListTitle>CONTRIBUTOR DIRECTORY</ContributorsListTitle>
+      <AliasesWrapper>
+        {aliases.map((alias, i) => (
+          <Alias key={'alias' + i}>{alias}</Alias>
+        ))}
+      </AliasesWrapper>
     </ContributorsListWrapper>
   );
 };
--- shared/styles.js
+++ shared/styles.js
@@ -14,6 +14,16 @@ export const globalStyles = (
         font-family: 'Resistance';
         src: url('/fonts/resistance.otf') format('opentype');
       }
+
+      @font-face {
+        font-family: 'Terminal';
+        src: url('/fonts/terminal-grotesque.ttf') format('truetype');
+      }
+
+      @font-face {
+        font-family: 'Favorit Pro';
+        src: url('/fonts/FavoritPro-Regular.otf') format('opentype');
+      }
     `}
   />
 );
