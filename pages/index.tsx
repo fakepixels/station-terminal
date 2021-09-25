7da8a80ed778e34d4f9ceb8236fdb17405813998
Tyler Sullberg <tysullberg@gmail.com>
2021-09-24 22:42:37 -0700

    Create DAO summary component.

--- pages/index.tsx
+++ pages/index.tsx
@@ -203,8 +203,120 @@ const DAOSummaryWrapper = styled.div`
   width: 355px;
 `;
 
+const DaoSummaryContibutionsWrapper = styled.div``;
+
+const DaoSummaryContibutionsTitle = styled.div`
+  font-family: Terminal;
+  font-size: 30px;
+  color: #f2efef;
+  margin: 20px 16px 0px;
+`;
+
+const DaoSummaryContibutionsGrid = styled.div`
+  display: grid;
+  grid-template-columns: auto auto;
+  grid-template-rows: 20px 20px;
+  color: #f2efef;
+  margin: 12px 16px 20px;
+`;
+
+const DaoSummaryLabel = styled.p`
+  color: #f2efef;
+  font-size: 14px;
+  font-family: Favorit Pro;
+`;
+
+const DaoSummaryValue = styled.p`
+  text-align: right;
+  color: #f2efef;
+  font-size: 14px;
+  font-family: Favorit Pro;
+`;
+
+const DaoSummaryToken = styled.span`
+  color: #626262;
+`;
+
+const DaoSummaryDivider = styled.div`
+  border-bottom: 1px solid #626262;
+  width: 100%;
+`;
+
+const DaoSummaryTextWrapper = styled.div`
+  margin: 20px 20px 16px 16px;
+`;
+
+const DAOSummaryText = styled.p`
+  color: #f2efef;
+  font-size: 14px;
+  font-family: Favorit Pro;
+`;
+
+const DocsLinkWrapper = styled.a`
+  display: flex;
+  flex-direction: row;
+`;
+
+const DocsLinkArrowIcon = styled.img`
+  margin-right: 10px;
+`;
+
+const DocsLinkText = styled.p`
+  color: #85d59a;
+  font-size: 14px;
+  font-family: Favorit Pro;
+  margin: 0px;
+`;
+
+const DocsLink = ({
+  linkTitle,
+  url,
+}: {
+  linkTitle: string;
+  url: string;
+}): JSX.Element => {
+  return (
+    <DocsLinkWrapper href={url} target="_blank">
+      <DocsLinkArrowIcon src={'/Arrow.svg'} alt={'arrow icon'} />
+      <DocsLinkText>{linkTitle}</DocsLinkText>
+    </DocsLinkWrapper>
+  );
+};
+
 const DAOSummary = (): JSX.Element => {
-  return <DAOSummaryWrapper></DAOSummaryWrapper>;
+  const week = 1;
+  const contributorCount = 25;
+  const contributorBudget = 400000;
+  const token = '$DEF';
+
+  return (
+    <DAOSummaryWrapper>
+      <DaoSummaryContibutionsWrapper>
+        <DaoSummaryContibutionsTitle>WEEK {week}</DaoSummaryContibutionsTitle>
+      </DaoSummaryContibutionsWrapper>
+      <DaoSummaryContibutionsGrid>
+        <DaoSummaryLabel>Contributors</DaoSummaryLabel>
+        <DaoSummaryValue>{contributorCount}</DaoSummaryValue>
+        <DaoSummaryLabel>Contributor budget</DaoSummaryLabel>
+        <DaoSummaryValue>
+          {contributorBudget} <DaoSummaryToken>{token}</DaoSummaryToken>
+        </DaoSummaryValue>
+      </DaoSummaryContibutionsGrid>
+      <DaoSummaryDivider />
+      <DaoSummaryTextWrapper>
+        <DAOSummaryText>
+          Default creates building blocks for governance, payments, and
+          peer-to-peer coordination mechanisms that align incentives across
+          organizational stakeholders and optimize for fairness and meritocracy.
+        </DAOSummaryText>
+        <DocsLink linkTitle={'Contributor Directory'} url={'/'} />
+        <DocsLink linkTitle={'Docs'} url={'/'} />
+        <DocsLink linkTitle={'Discord'} url={'/'} />
+        <DocsLink linkTitle={'Twitter'} url={'/'} />
+        <DocsLink linkTitle={'Github'} url={'/'} />
+      </DaoSummaryTextWrapper>
+    </DAOSummaryWrapper>
+  );
 };
 
 const Home = (): JSX.Element => {
