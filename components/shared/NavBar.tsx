1887b6ede364bfb2ac15db007d4805bde33826df
Tyler Sullberg <tysullberg@gmail.com>
2021-09-27 09:02:41 -0700

    Fix styling of navbar and make it mobile responsive.

--- components/shared/NavBar.tsx
+++ components/shared/NavBar.tsx
@@ -36,17 +36,56 @@ const ControlWrapper = styled.div`
 `;
 
 const DropdownWrapper = styled.div`
-  background-color: #6a6dcd;
-  color: white;
   height: 100%;
-  width: 259px;
   display: flex;
+  flex-direction: row;
   justify-content: center;
   align-items: center;
+  background-color: #f2efef;
+  font-family: Resistance;
+  font-size: 30px;
+  border-left: 1px solid #090909;
+  padding: 0px 24px;
 `;
 
-const Dropdown = ({ address }: { address: string }) => {
-  return <DropdownWrapper>{address}</DropdownWrapper>;
+const DropdownImage = styled.img`
+  border-radius: 50%;
+  border: 1px solid #090909;
+  height: 36px;
+  width: 36px;
+  margin-right: 10px;
+`;
+
+const DropownAddress = styled.div`
+  @media (max-width: 540px) {
+    display: none;
+  }
+`;
+
+const Dropdown = ({
+  address,
+  avatarURL,
+}: {
+  address: string;
+  avatarURL: string;
+}) => {
+  const truncateAddress = (address: string): string => {
+    if (!address) return '';
+
+    const length = address.length;
+    return (
+      address.substring(0, 4) + '...' + address.substring(length - 4, length)
+    );
+  };
+
+  const truncatedAddress = truncateAddress(address);
+
+  return (
+    <DropdownWrapper>
+      <DropdownImage src={avatarURL} />
+      <DropownAddress>{truncatedAddress}</DropownAddress>
+    </DropdownWrapper>
+  );
 };
 
 const NavBar = (): JSX.Element => {
@@ -60,7 +99,10 @@ const NavBar = (): JSX.Element => {
         <StationLogo src="/StationLogo.svg" alt="Station Logo" />
       </LogoWrapper>
       <ControlWrapper>
-        <Dropdown address={'0xfakeaddress'} />
+        <Dropdown
+          address={'0xfakeaddress'}
+          avatarURL={'./sampleAssets/Hobbes.png'}
+        />
       </ControlWrapper>
     </Wrapper>
   );
